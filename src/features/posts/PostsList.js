fc93a071e0cfde523dac90bcb152ba636cfd919b
Kijasss <95722807@qq.com>
2022-07-09 16:39:10 +0800

    Use createEntityAdapter to rewrite PostsSlice & PostsList

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,11 +3,13 @@ import { Spinner } from '../../components/Spinner'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import PostAuthor from './PostAuthor'
-import { fetchPosts, selectAllPosts } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
+import { fetchPosts, selectPostIds, selectPostById } from './postsSlice'
 
-const PostExcerpt = ({ post }) => {
+const PostExcerpt = ({ postId }) => {
+  const post = useSelector((state) => selectPostById(state, postId))
+
   return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
@@ -27,7 +29,7 @@ const PostExcerpt = ({ post }) => {
 
 export const PostsList = () => {
   const dispatch = useDispatch()
-  const posts = useSelector(selectAllPosts)
+  const orderedPostIds = useSelector(selectPostIds)
 
   const postStatus = useSelector((state) => state.posts.status)
   const error = useSelector((state) => state.posts.error)
@@ -44,12 +46,8 @@ export const PostsList = () => {
     content = <Spinner text="Loading..." />
   } else if (postStatus === 'succeeded') {
     // Sort posts in reverse chronological order by datetime string
-    const orderedPosts = posts
-      .slice()
-      .sort((a, b) => b.date.localeCompare(a.date))
-
-    content = orderedPosts.map((post) => (
-      <PostExcerpt key={post.id} post={post} />
+    content = orderedPostIds.map((postId) => (
+      <PostExcerpt key={postId} postId={postId} />
     ))
   } else if (postStatus === 'failed') {
     content = <div>{error}</div>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -3,14 +3,33 @@ import {
   createSlice,
   nanoid,
   createSelector,
+  createEntityAdapter,
 } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
 
-const initialState = {
-  posts: [],
+// “范式化 state”是指：
+
+// 我们 state 中的每个特定数据只有一个副本，不存在重复。
+// 已范式化的数据保存在查找表中，其中项目 ID 是键，项本身是值。
+// 也可能有一个特定项用于保存所有 ID 的数组。
+
+// 这使得通过 ID 查找特定的“用户”对象变得简单，不需遍历数组中其他的用户对象：
+// const userId = 'user2'
+// const userObject = state.users.entities[userId]
+
+// 使用' createEntityAdapter '管理范式化 state
+
+const postsAdapter = createEntityAdapter({
+  sortComparer: (a, b) => b.date.localeCompare(a.date),
+})
+
+// getInitialState 函数，它生成一个空的 {ids: [], entities: {}} 对象。
+// 您可以传递更多字段给 getInitialState，这些字段将会被合并。
+
+const initialState = postsAdapter.getInitialState({
   status: 'idle',
   error: null,
-}
+})
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -44,14 +63,14 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.posts.find((post) => post.id === postId)
+      const existingPost = state.entities[postId]
       if (existingPost) {
         existingPost.reactions[reaction]++
       }
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
-      const existingPost = state.posts.find((post) => id === post.id)
+      const existingPost = state.entities[id]
       if (existingPost) {
         existingPost.title = title
         existingPost.content = content
@@ -66,15 +85,18 @@ const postsSlice = createSlice({
       .addCase(fetchPosts.fulfilled, (state, action) => {
         state.status = 'succeeded'
         // Add any fetched posts to the array
-        state.posts = state.posts.concat(action.payload)
+        // upsertMany 函数将根据匹配的 ID 将它们合并在一起。
+        // state.posts = state.posts.concat(action.payload)
+        postsAdapter.upsertMany(state, action.payload)
       })
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = 'failed'
         state.error = action.error.message
       })
-      .addCase(addNewPost.fulfilled, (state, action) => {
-        state.posts.push(action.payload)
-      })
+      // 我们需要将一个新的帖子对象添加到我们的 state 中。
+      // 我们可以直接将 adapter 用作 reducer，
+      // 所以我们将传递 postsAdapter.addOne 作为 reducer 函数来处理该操作。
+      .addCase(addNewPost.fulfilled, postsAdapter.addOne)
   },
 })
 
@@ -82,10 +104,14 @@ export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
 
-export const selectAllPosts = (state) => state.posts.posts
+// 最后，我们可以用 postsAdapter.getSelectors 生成的 selector 函数
+// 替换旧的手写的 selectAllPosts 和 selectPostById selector 函数。
 
-export const selectPostById = (state, postId) =>
-  state.posts.posts.find((post) => post.id === postId)
+export const {
+  selectAll: selectAllPosts,
+  selectById: selectPostById,
+  selectIds: selectPostIds,
+} = postsAdapter.getSelectors((state) => state.posts)
 
 export const selectPostsByUser = createSelector(
   [selectAllPosts, (state, userId) => userId],
